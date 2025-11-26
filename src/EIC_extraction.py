8366bace33e9a9f45cdf115547f8ab9c7f0cc29b
awwadbasel0 <awwadbasel0@gmail.com>
2025-11-26 11:14:19 +0100

    added different fit method

--- src/EIC_extraction.py
+++ src/EIC_extraction.py
@@ -7,6 +7,7 @@ import scipy.signal as sig
 import argparse
 import pathlib
 from scipy.optimize import curve_fit,least_squares
+import lmfit
 import os
 
 
@@ -54,6 +55,7 @@ def get_final_eic_intensities(spectra, min_mz, max_mz):
 
 def gaus(x, a, x0, sigma):
     return a * np.exp(-(x - x0)**2 / ( sigma**2))
+
 def fit_curve(y):
     x=np.arange(1,len(y)+1)
     mu=np.mean(x)
@@ -62,8 +64,16 @@ def fit_curve(y):
     params, error = curve_fit(gaus, x, y,p0=initial_guess,method='trf',nan_policy='omit')
     fit=gaus(x,*params)
     return fit
-def dip_detect_correct(y):
+
+#finds the peaks of the smoothed graph
+#finds the smallest value between first and last peak: min
+#then sets the mid point of interpolation to double of: biggest peak - min
+#interpolates between first peak, mid point, last peak quadratically
+#returns graph with dip replaced with interpolated values
+def dip_detect_correct(y,peaks):
     peak, _ = sig.find_peaks(y, height=np.mean(y))
+    # peak=peaks
+    plt.plot(peak,y[peak],"x",f"Eic's detected Peaks")
     max_peak=max(y[peak])
     minPoint = max_peak + (max_peak - min(y[peak[0]:peak[-1]]))
     pointsX=np.array([peak[0] - 1,
@@ -76,11 +86,32 @@ def dip_detect_correct(y):
     interpolated = np.concatenate((y[:(peak[0] - 1)], predict(np.arange(1,len(y))[peak[0] - 1:peak[len(peak) - 1] + 1]),
                                            y[peak[len(peak) - 1] + 1:]))
     return interpolated
+
+#returns where in general ion suppression occurs
+def new_dip_suppression_detect(Y:np.ndarray[float]):
+    # smoothed_ys=[sig.savgol_filter(y,30,3) for y in Y]
+    peak,_=sig.find_peaks(Y,height=Y.mean())
+    return peak
 def try_flip(y):
     peak,_=sig.find_peaks(y,height=np.mean(y))
     max_peak=max(y[peak])
     result=np.concatenate((y[:peak[0]],[x+((y[peak[0]]-x)*2) for x in y[peak[0]:(peak[-1]-int((peak[-1]-peak[0])/2))]],[x+((y[peak[-1]]-x)*2) for x in y[peak[-1]-int((peak[-1]-peak[0])/2):peak[-1]]],y[peak[-1]:]))
     return result
+
+def gauss_constant_fit(y:list):
+    # mod = lmfit.models.GaussianModel() + lmfit.models.ConstantModel()
+    mod = lmfit.models.GaussianModel(prefix="g1_") + lmfit.models.ExponentialModel(prefix="exp_")
+    xdat=np.arange(1,len(y)+1)
+
+    pars = mod.make_params(g1_c=np.mean(y),
+                           g1_center=xdat.mean(),
+                           g1_sigma=xdat.std(),
+                           g1_amplitude=xdat.std() * np.ptp(y),
+                           )
+
+    out = mod.fit(y, pars, x=xdat)
+    return out.best_fit
+
 def main():
 
     parser = argparse.ArgumentParser(
@@ -140,9 +171,10 @@ def main():
     print(f"Loading MS1 file: {path}")
     spectra = load_ms1(path)
     print(f"Loaded {len(spectra)} spectra.")
-
+    whole_region_smoothed=sig.savgol_filter(get_final_eic_intensities(spectra,-np.inf,np.inf),30,3)
+    supp_occur=new_dip_suppression_detect(whole_region_smoothed)
     # Extract EICs for each region
-
+    plt.style.use('ggplot')
     for (i,(min_mz, max_mz)) in enumerate(regions):
         #
         final_intensities = get_final_eic_intensities(spectra, min_mz, max_mz)
@@ -159,15 +191,15 @@ def main():
 
 
         if args.interpolate:
-            dip_corrected_intensities=dip_detect_correct(smoothed_intensities)
+            dip_corrected_intensities=dip_detect_correct(smoothed_intensities,supp_occur)
             plt.plot(seconds,dip_corrected_intensities, label=f"EIC's Dip Correction")
 
         if args.fit:
             if args.interpolate:
-                fitted_intensities=fit_curve(dip_corrected_intensities)
+                fitted_intensities=gauss_constant_fit(dip_corrected_intensities)
             else:
-                fitted_intensities=fit_curve(smoothed_intensities)
-            plt.plot(seconds, fitted_intensities,'--',label=f"EIC's Gaussian Fit")
+                fitted_intensities=gauss_constant_fit(smoothed_intensities)
+            plt.plot(seconds, fitted_intensities,"--",label=f"EIC's Gaussian Fit")
 
         plt.xlabel("Seconds")
         plt.ylabel("Total intensity")
